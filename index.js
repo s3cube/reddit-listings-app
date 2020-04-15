dd94df70ec4ec8401198114e0c74e2518c4c0003
Satej Sawant <satejs93@gmail.com>
2020-04-15 19:46:07 +0530

    Added Package Lock file

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
+
+const axios = require("axios").default;
+
+
+let apiResponse;
+
 exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
         body: JSON.stringify('Hello from Lambda!!'),
     };
+
+    axios.get("https://www.reddit.com/r/UpliftingNews/.json")
+    .then((response, error)=>{
+        console.log(response);
+        console.log(error);
+        apiResponse = response;
+    });
+
+    console.log(apiResponse)
+
     return response;
 };
